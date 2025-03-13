cc22c84aefae513ea34540f4052dc373a0915b87
Rafael <rafaelsdias1@gmail.com>
2025-03-13 15:44:07 +0000

    api google- sem api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation(libs.okhttp)
 
 
+
 }
--- app/src/main/java/com/example/samu/MapsActivity.kt
+++ app/src/main/java/com/example/samu/MapsActivity.kt
@@ -1,156 +1,75 @@
 package com.example.samu
 
-import android.location.Geocoder
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import org.osmdroid.api.IMapController
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Polyline
-import org.osmdroid.util.GeoPoint
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class MapsActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mapView: MapView
-    private lateinit var searchViewOrigin: SearchView
-    private lateinit var searchViewDestination: SearchView
-    private lateinit var routeManager: RouteManager
-    private lateinit var startLatLng: GeoPoint
-    private lateinit var endLatLng: GeoPoint
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        mapView = findViewById(R.id.mapView)
-        mapView.setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK)  // Configure o tile
-        mapView.setBuiltInZoomControls(true)  // Habilitar controles de zoom
-        mapView.setMultiTouchControls(true)  // Habilitar multi-touch para controle de zoom
+        // Inicializar o FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        searchViewOrigin = findViewById(R.id.searchViewOrigin)
-        searchViewDestination = findViewById(R.id.searchViewDestination)
-        routeManager = RouteManager()
+        // Inicializar o MapFragment e obter o mapa
+        val mapFragment = fragmentManager.findFragmentById(R.id.map) as MapFragment
+        mapFragment.getMapAsync(this)
+    }
 
-        // Configuração do mapa
-        mapView.setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK)
-        mapView.setBuiltInZoomControls(true)
-        mapView.setMultiTouchControls(true)
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
-        // Botão de Traçar Rota
-        findViewById<Button>(R.id.btnTraçarRota).setOnClickListener {
-            if (::startLatLng.isInitialized && ::endLatLng.isInitialized) {
-                // Traçar rota
-                routeManager.getRoute(
-                    listOf(startLatLng.longitude, startLatLng.latitude),
-                    listOf(endLatLng.longitude, endLatLng.latitude)
-                ) { routeResponse ->
-                    if (routeResponse != null) {
-                        // Adicionar a rota ao mapa
-                        val polyline = Polyline()
-                        for (coord in routeResponse.routes[0].geometry.coordinates) {
-                            polyline.addPoint(GeoPoint(coord[1], coord[0]))
-                        }
-                        mapView.overlayManager.add(polyline)
-
-                        // Configurar a posição do mapa
-                        val controller: IMapController = mapView.controller
-                        controller.setZoom(12)
-                        controller.setCenter(startLatLng) // Centra o mapa na origem
-                    } else {
-                        Toast.makeText(this, "Erro ao obter a rota", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Por favor, defina ambos os locais de partida e destino", Toast.LENGTH_SHORT).show()
-            }
+        // Verificar permissões de localização
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Solicitar permissões se necessário
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            return
         }
 
-        // Inicializa as buscas
-        setupSearchViews()
-    }
+        // Habilitar a localização no mapa
+        mMap.isMyLocationEnabled = true
 
-    private fun setupSearchViews() {
-        // Ação para a barra de pesquisa de origem
-        searchViewOrigin.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (!query.isNullOrEmpty()) {
-                    // Definir local de origem
-                    setLocation(query, true)
-                }
-                return false
+        // Obter a localização do usuário
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                val userLocation = LatLng(it.latitude, it.longitude)
+
+                // Centralizar o mapa na localização do usuário e adicionar um marcador
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                mMap.addMarker(MarkerOptions().position(userLocation).title("A tua localização"))
+            } ?: run {
+                Toast.makeText(this, "Não foi possível obter a localização", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-
-        // Ação para a barra de pesquisa de destino
-        searchViewDestination.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (!query.isNullOrEmpty()) {
-                    // Definir local de destino
-                    setLocation(query, false)
-                }
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-    }
-
-    private fun setLocation(location: String, isStart: Boolean) {
-        val geocoder = Geocoder(this)
-        try {
-            val addresses = geocoder.getFromLocationName(location, 1)
-            if (addresses != null && addresses.isNotEmpty()) {
-                val address = addresses[0]
-                val latLng = GeoPoint(address.latitude, address.longitude)
-                if (isStart) {
-                    startLatLng = latLng
-                    val startMarker = Marker(mapView)
-                    startMarker.position = latLng
-                    startMarker.title = "Origem"
-                    mapView.overlayManager.add(startMarker)
-                } else {
-                    endLatLng = latLng
-                    val endMarker = Marker(mapView)
-                    endMarker.position = latLng
-                    endMarker.title = "Destino"
-                    mapView.overlayManager.add(endMarker)
-                }
-
-                // Ajustar o centro do mapa para o local
-                val controller: IMapController = mapView.controller
-                controller.setZoom(12)
-                controller.setCenter(latLng)
-            } else {
-                Toast.makeText(this, "Local não encontrado", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "Erro ao procurar o local", Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()  // Chama o método do OSMDroid para retomar o mapa
+    // Gerenciar permissões em tempo de execução
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Permissão concedida, inicializar o mapa
+            onMapReady(mMap)
+        } else {
+            Toast.makeText(this, "Permissão de localização não concedida", Toast.LENGTH_SHORT).show()
+        }
     }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()  // Chama o método do OSMDroid para pausar o mapa
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDetach()  // Chama o método para limpar o mapa
-    }
-
-
 }
+
